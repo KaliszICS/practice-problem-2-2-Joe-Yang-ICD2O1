6f0010a43c9d9bcce4f249ada8592f3b25b507ff
Joe-Yang-ICD2O1 <joe.yang@student.tdsb.on.ca>
2024-10-15 14:19:13 +0000

    answer

--- PP2_2.py
+++ PP2_2.py
@@ -3,17 +3,44 @@
 def q1(): 
   #Write Assignment code here
 
+  num= int(input("Input an integer: "))
+
+  if num== 5:
+    print("The number is Five")
+  else:
+    print("The number is not Five")
 
 def q2(): 
   #Write Assignment code here
 
+  num= float(input("Input a number: "))
+
+  if num>= 0:
+    print("Positive")
+
+  else: 
+    print("Negative")
+
 def q3(): 
   #Write Assignment code here
+  num= int(input("Input an integer: "))
+
+  if num % 2 == 0:
+    print("Even")
+
+  else: 
+    print("Odd")
+
 
 def q4(): 
   #Write Assignment code here
-
-
+  num= input('Type "Hello": ')
+  
+  if num == "Hello": 
+    print("The word is Hello")
+  
+  else:
+    print("The word is not Hello")
 
 
 #Do not alter the following code
